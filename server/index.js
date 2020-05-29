6d7b16473e44eab3a91b9febdb050187bb55dfad
pero <lipotic.pero@gmail.com>
2020-05-29 11:54:27 +0200

    finished node auth

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const router = require("./router");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:auth/auth", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("SERVER LISTENING ON: " + port);
